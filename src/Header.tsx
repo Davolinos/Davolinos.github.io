ecf19743a9410d1ffc07db6188c8cde8f0e98341
Dafos <daffahudaya9@gmail.com>
2024-02-17 17:57:36 +0700

    apdet ajah

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+
+function Header() {
+    return (
+        <div className="flex top-0 right-0 h-20 bg-slate-300 justify-around items-center">
+            <h1 className="font-bold text-lg">
+                Hudaya's <span>Note</span>
+            </h1>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Header;
