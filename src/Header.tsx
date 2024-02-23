7614ef2926604b892fc03fbaa2ecbe0683ce3ff7
Dafos <daffahudaya9@gmail.com>
2024-02-23 18:03:05 +0700

    buat dipush

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <div className="flex top-0 right-0 h-20 bg-slate-300 justify-around items-center">
-            <h1 className="font-bold text-lg">
+            <Link className="font-bold text-lg" to={'/'}>
                 Hudaya's <span>Note</span>
-            </h1>
-            <div>
-
-            </div>
+            </Link>
+                <Link className="mg-10" to={'articles'}>Articles</Link>
+                <Link className="mg-10" to={'stories'}>Stories</Link>
+                <Link className="mg-10" to={'about'}>About</Link>
         </div>
     );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,29 @@
 import Header from "./Header";
 
+interface untukKotak {
+    judul: String;
+    deskripsi: String;
+}
+
+function Kotak({judul, deskripsi}: untukKotak) {
+    return (
+        <div className="flex justify-center items-center flex-column w-[200px] h-[300px] bg-red-200">    
+            <h1>{judul}</h1>
+            <p>{deskripsi}</p>
+        </div>
+    );
+}
+
 function Home() {
     return (
         <>
-        <Header />
+            <Header />
+            <div className="flex items-center justify-around">
+                <Kotak judul={"1"} deskripsi={"awikwok"}/>
+                <Kotak judul={"1"} deskripsi={"awikwok"}/>
+                <Kotak judul={"1"} deskripsi={"awikwok"}/>
+                <Kotak judul={"1"} deskripsi={"awikwok"}/>
+            </div>
         </>
     );
 }
