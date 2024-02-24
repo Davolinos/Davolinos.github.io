3b7b6f5400843505dcc8aa9fe47b769120cef44b
Davolinos <daffahudaya9@gmail.com>
2024-02-24 14:00:58 +0700

    pake linux euy

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className="flex top-0 right-0 h-20 bg-slate-300 justify-around items-center">
-            <Link className="font-bold text-lg" to={'/'}>
-                Hudaya's <span>Note</span>
+        <div className="flex top-0 right-0 h-20 justify-around items-center">
+            <Link className="font-bold text-2xl" to={'/'}>
+                Hudaya's <span className="text-red-500">Note</span>
             </Link>
-                <Link className="mg-10" to={'articles'}>Articles</Link>
-                <Link className="mg-10" to={'stories'}>Stories</Link>
-                <Link className="mg-10" to={'about'}>About</Link>
+                <Link className="mg-10 font-bold" to={'articles'}>Articles</Link>
+                <Link className="mg-10 font-bold" to={'stories'}>Stories</Link>
+                <Link className="mg-10 font-bold" to={'about'}>About</Link>
         </div>
     );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,27 @@
 import Header from "./Header";
+import { FaPerson } from "react-icons/fa6";
+import { TbWriting } from "react-icons/tb";
+import { FaRegNewspaper } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 interface untukKotak {
-    judul: String;
+    judul: React.ReactNode;
     deskripsi: String;
+	link: String;
 }
 
-function Kotak({judul, deskripsi}: untukKotak) {
+function Kotak(props: untukKotak) {
     return (
-        <div className="flex justify-center items-center flex-column w-[200px] h-[300px] bg-red-200">    
-            <h1>{judul}</h1>
-            <p>{deskripsi}</p>
+		<Link to={`{props.link}`}>
+        <div className="rounded-xl flex justify-center items-center w-[200px] h-[100px] hover:scale-110 transition-all duration-200 bg-red-200">    
+			<div className="text-center">
+				<div className="flex justify-center items-center">
+					{props.judul}
+				</div>
+				<p>{props.deskripsi}</p>
+			</div>
         </div>
+		</Link>
     );
 }
 
@@ -18,11 +29,10 @@ function Home() {
     return (
         <>
             <Header />
-            <div className="flex items-center justify-around">
-                <Kotak judul={"1"} deskripsi={"awikwok"}/>
-                <Kotak judul={"1"} deskripsi={"awikwok"}/>
-                <Kotak judul={"1"} deskripsi={"awikwok"}/>
-                <Kotak judul={"1"} deskripsi={"awikwok"}/>
+            <div className="flex items-center justify-around h-[500px]">
+                <Kotak link="about" judul={<FaPerson />} deskripsi={"About Me"}/>
+                <Kotak link="articles" judul={<FaRegNewspaper />} deskripsi={"My Journal"}/>
+                <Kotak link="stories" judul={<TbWriting />} deskripsi={"Stories"}/>
             </div>
         </>
     );
